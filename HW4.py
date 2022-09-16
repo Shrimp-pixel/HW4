5c225bda0e968ba390e31fcb0b35ac44015b9c75
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-09-16 17:33:08 +0300

    Init commit

---
+++ HW4.py
@@ -0,0 +1,20 @@
+def func1(function, *args):
+    print('Function name:', function.__name__)
+    print("Args:", ', '.join(args))
+
+
+def open_browser(browser_name):
+    func1(open_browser, browser_name)
+
+
+def go_to_companyname_homepage(page_url):
+    func1(go_to_companyname_homepage, page_url)
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    func1(find_registration_button_on_login_page, page_url, button_text)
+
+
+open_browser('Browser#1')
+go_to_companyname_homepage('https://valid.url.com')
+find_registration_button_on_login_page('https://valid.url.com', 'Text#1')
